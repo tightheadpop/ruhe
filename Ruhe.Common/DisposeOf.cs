4db871851de5abc01ed40ddebe3a9d1fdfa92971
matthew.a.frank <matthew.a.frank@d311d79a-a41b-0410-be46-71240311c8d0>
2008-02-12 17:53:18 +0000

    DisoposeOf.These should not stifle exceptions

--- Ruhe.Common/DisposeOf.cs
+++ Ruhe.Common/DisposeOf.cs
@@ -8,8 +8,8 @@ namespace Ruhe.Common {
     /// Utility for disposing cleanly of items
     /// </summary>
     public class DisposeOf {
-        private static readonly string[] DisposingMethods = new string[] { "Flush", "Close", "Dispose" };
-        private DisposeOf() { }
+        private static readonly string[] DisposingMethods = new string[] {"Flush", "Close", "Dispose"};
+        private DisposeOf() {}
 
         /// <summary>
         /// Handles common clean up tasks, squelching exceptions.
@@ -18,7 +18,7 @@ namespace Ruhe.Common {
         /// Flush, Close, Dispose, in that order.
         /// </remarks>
         public static void These(params object[] stuff) {
-            These((IEnumerable)stuff);
+            These((IEnumerable) stuff);
         }
 
         public static void These(IEnumerable items) {
@@ -38,7 +38,7 @@ namespace Ruhe.Common {
         private static IEnumerable<MethodInfo> GetDisposingMethods(object obj) {
             List<MethodInfo> methods = new List<MethodInfo>(DisposingMethods.Length);
             foreach (string methodName in DisposingMethods) {
-                methods.Add(obj.GetType().GetMethod(methodName, new Type[] { }));
+                methods.Add(obj.GetType().GetMethod(methodName, new Type[] {}));
             }
             return methods;
         }
@@ -49,8 +49,8 @@ namespace Ruhe.Common {
                     try {
                         method.Invoke(obj, null);
                     }
-                    catch {
-                        //do nothing
+                    catch (Exception e) {
+                        throw e.InnerException;
                     }
                 }
             }
