27e78367365fca294753a7940c93e1eaeaada6e4
matthew.a.frank <matthew.a.frank@d311d79a-a41b-0410-be46-71240311c8d0>
2008-03-08 20:01:05 +0000

    Validate with convenience methods using string.Format

--- Ruhe.Common/Validate.cs
+++ Ruhe.Common/Validate.cs
@@ -8,8 +8,13 @@ namespace Ruhe.Common {
         protected static Type exceptionType = typeof(T);
 
         public static void HasNoNullElements(IEnumerable list, string errorMessage) {
+            IsNotNull(list, "list cannot be null");
+            HasNoNullElements(list, errorMessage, new object[0]);
+        }
+
+        public static void HasNoNullElements(IEnumerable list, string errorMessageFormat, params object[] parameters) {
             foreach (object o in list) {
-                IsNotNull(o, errorMessage);
+                IsNotNull(o, string.Format(errorMessageFormat, parameters));
             }
         }
 
@@ -17,31 +22,43 @@ namespace Ruhe.Common {
             That(!expression, errorMessage);
         }
 
+        public static void IsFalse(bool expression, string errorMessageFormat, params object[] parameters) {
+            IsFalse(expression, string.Format(errorMessageFormat, parameters));
+        }
+
         public static void IsNotEmpty(IEnumerable list, string errorMessage) {
             IsNotNull(list, errorMessage);
-#pragma warning disable 168
-            foreach (object o in list) {
-#pragma warning restore 168
-                return;
-            }
+            if (list.GetEnumerator().MoveNext()) return;
             ThrowNewException(errorMessage);
+        }
+
+        public static void IsNotEmpty(IEnumerable list, string errorMessageFormat, params object[] parameters) {
+            IsNotEmpty(list, string.Format(errorMessageFormat, parameters));
         }
 
         public static void IsNotNull(object obj, string errorMessage) {
             That(obj != null, errorMessage);
         }
 
-        public static void IsNull(object obj, string errorMessage) {
-            That(obj == null, errorMessage);
+        public static void IsNotNull(object obj, string errorMessageFormat, params object[] parameters) {
+            IsNotNull(obj, string.Format(errorMessageFormat, parameters));
         }
 
         public static void IsTrue(bool expression, string errorMessage) {
             That(expression, errorMessage);
         }
 
+        public static void IsTrue(bool expression, string errorMessageFormat, params object[] parameters) {
+            IsTrue(expression, string.Format(errorMessageFormat, parameters));
+        }
+
         public static void That(bool expression, string errorMessage) {
             if (!expression)
                 ThrowNewException(errorMessage);
+        }
+
+        public static void That(bool expression, string errorMessageFormat, params object[] parameters) {
+            That(expression, string.Format(errorMessageFormat, parameters));
         }
 
         private static void ThrowNewException(string errorMessage) {
