2370541eaafef1414876d2c281042f68f0f3f6bc
matthew.a.frank <matthew.a.frank@d311d79a-a41b-0410-be46-71240311c8d0>
2009-01-18 20:47:10 +0000

    playing with DateTime features

--- trunk/Ruhe.Common/Utilities/DateTimeUtilities.cs
+++ trunk/Ruhe.Common/Utilities/DateTimeUtilities.cs
@@ -11,35 +11,28 @@ namespace Ruhe.Common.Utilities {
     /// Function bucket providing common DateTime utility methods
     /// </summary>
     public static class DateTimeUtilities {
-        public static long DifferenceInMonths(this DateTime date1, DateTime date2) {
-            return date1.DifferenceInMonths(date2, RoundingOption.Down);
+        public static DateTime After(this TimeSpan span, DateTime start) {
+            return start.Add(span);
         }
 
-        public static long DifferenceInMonths(this DateTime date1, DateTime date2, RoundingOption roundingOption) {
-            long differenceInMonths = DateAndTime.DateDiff(DateInterval.Month, date1, date2, FirstDayOfWeek.Sunday, FirstWeekOfYear.Jan1);
-
-            if (roundingOption == RoundingOption.Up) {
-                DateTime tempDate = date1.AddMonths((int) differenceInMonths);
-                long differenceInDays = DateAndTime.DateDiff(DateInterval.Day, tempDate, date2, FirstDayOfWeek.Sunday, FirstWeekOfYear.Jan1);
-                if (differenceInDays > 0) {
-                    differenceInMonths++;
-                }
-            }
-            return differenceInMonths;
+        public static DateTime Ago(this TimeSpan span) {
+            return DateTime.Now.Add(-span);
         }
 
-        public static DateTime GetFirstDayOfWeek(this DateTime dateTime) {
+        public static DateTime Before(this TimeSpan span, DateTime start) {
+            return start.Add(-span);
+        }
+
+        public static TimeSpan Days(this int days) {
+            return new TimeSpan(days, 0, 0, 0);
+        }
+
+        public static DateTime FirstDayOfWeek(this DateTime dateTime) {
             return dateTime.AddDays((int) dateTime.DayOfWeek * -1);
         }
 
-        public static DateTime GetLastDayOfWeek(this DateTime dateTime) {
-            return dateTime.GetFirstDayOfWeek().AddDays(6);
-        }
-
-        public static DateTime GetNext(this DateTime start, DayOfWeek dayOfWeek) {
-            if (dayOfWeek > start.DayOfWeek)
-                return start.AddDays(dayOfWeek - start.DayOfWeek);
-            return start.AddDays(7).GetFirstDayOfWeek().AddDays((int) dayOfWeek);
+        public static DateTime FromNow(this TimeSpan span) {
+            return span.After(DateTime.Now);
         }
 
         public static DateTime GetNthDayOfWeekInMonth(this int n, DayOfWeek dayOfWeek, Month month, int year) {
@@ -49,17 +42,56 @@ namespace Ruhe.Common.Utilities {
             DateTime startingDate = new DateTime(year, (int) month, 1).AddDays(-1);
             DateTime result = startingDate;
             for (int i = 0; i < n; i++)
-                result = result.GetNext(dayOfWeek);
+                result = result.Next(dayOfWeek);
             return result;
         }
 
-        public static DateTime GetPrevious(this DateTime start, DayOfWeek dayOfWeek) {
-            return start.AddDays(-8).GetNext(dayOfWeek);
+        public static TimeSpan Hours(this int hours) {
+            return new TimeSpan(0, hours, 0, 0);
         }
 
         public static bool IsDate(this string date) {
             DateTime result;
             return DateTime.TryParse(date, out result);
+        }
+
+        public static DateTime LastDayOfWeek(this DateTime dateTime) {
+            return dateTime.FirstDayOfWeek().AddDays(6);
+        }
+
+        public static TimeSpan Minutes(this int minutes) {
+            return new TimeSpan(0, 0, minutes, 0);
+        }
+
+        public static long MonthsBetween(this DateTime date1, DateTime date2) {
+            return date1.MonthsBetween(date2, RoundingOption.Down);
+        }
+
+        public static long MonthsBetween(this DateTime date1, DateTime date2, RoundingOption roundingOption) {
+            long differenceInMonths = DateAndTime.DateDiff(DateInterval.Month, date1, date2, Microsoft.VisualBasic.FirstDayOfWeek.Sunday, FirstWeekOfYear.Jan1);
+
+            if (roundingOption == RoundingOption.Up) {
+                DateTime tempDate = date1.AddMonths((int) differenceInMonths);
+                long differenceInDays = DateAndTime.DateDiff(DateInterval.Day, tempDate, date2, Microsoft.VisualBasic.FirstDayOfWeek.Sunday, FirstWeekOfYear.Jan1);
+                if (differenceInDays > 0) {
+                    differenceInMonths++;
+                }
+            }
+            return differenceInMonths;
+        }
+
+        public static DateTime Next(this DateTime start, DayOfWeek dayOfWeek) {
+            if (dayOfWeek > start.DayOfWeek)
+                return start.AddDays(dayOfWeek - start.DayOfWeek);
+            return start.AddDays(7).FirstDayOfWeek().AddDays((int) dayOfWeek);
+        }
+
+        public static DateTime Previous(this DateTime start, DayOfWeek dayOfWeek) {
+            return start.AddDays(-8).Next(dayOfWeek);
+        }
+
+        public static TimeSpan Seconds(this int seconds) {
+            return new TimeSpan(0, 0, 0, seconds);
         }
     }
 }
