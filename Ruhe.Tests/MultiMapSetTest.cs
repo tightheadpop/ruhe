8291852695e6d745f0999828d470a01e418fff4d
matthew.a.frank <matthew.a.frank@d311d79a-a41b-0410-be46-71240311c8d0>
2009-08-21 15:45:04 +0000

    completed core features of MultiMapSet

--- Ruhe.Tests/MultiMapSetTest.cs
+++ Ruhe.Tests/MultiMapSetTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using LiquidSyntax.ForTesting;
 using NUnit.Framework;
@@ -7,54 +8,64 @@ namespace Ruhe.Tests {
     public class MultiMapSetTest {
         [Test]
         public void ShouldAcceptSameKeyMoreThanOnceYouStupidFramework() {
-            var set = new MultiMapSet<int, int>();
-            set.Add(1, 1);
-            set.Add(1, 1);
+            var set = new MultiMapSet<int, int> {{1, 1}, {1, 1}};
 
             set[1].Should(Be.EquivalentTo(new[] {1}));
         }
 
         [Test]
         public void ShouldAcceptMultipleValuesForAGivenKey() {
-            var set = new MultiMapSet<int, int>();
-            set.Add(1, 1);
-            set.Add(1, 11);
+            var set = new MultiMapSet<int, int> {{1, 1}, {1, 11}};
 
             set[1].Should(Be.EquivalentTo(new[] {1, 11}));
         }
 
         [Test]
         public void ShouldAcceptMultipleKeys() {
-            var set = new MultiMapSet<int, int>();
-            set.Add(1, 1);
-            set.Add(2, 11);
+            var set = new MultiMapSet<int, int> {{1, 1}, {2, 11}};
 
             set.Keys.Should(Be.EquivalentTo(new[] {1, 2}));
         }
 
         [Test]
         public void ShouldProvideCollectionOfSetsOfValues() {
-            var set = new MultiMapSet<int, int>();
-            set.Add(1, 1);
-            set.Add(2, 45726);
-            set.Add(1, 11);
-            set.Add(3, 6);
+            var set = new MultiMapSet<int, int> {{1, 1}, {2, 45726}, {1, 11}, {3, 6}};
 
             set.Values.Should(Be.EquivalentTo(new[] {
-                new HashSet<int> {1, 11}, 
-                new HashSet<int> {45726}, 
+                new HashSet<int> {1, 11},
+                new HashSet<int> {45726},
                 new HashSet<int> {6}
             }));
         }
 
         [Test]
         public void ShouldBeAbleToTellYouIfItContainsKey() {
-            var set = new MultiMapSet<int, int>();
-            set.Add(1, 1);
-            set.Add(2, 4);
+            var set = new MultiMapSet<int, int> {{1, 1}, {2, 4}};
 
             set.ContainsKey(1).Should(Be.True);
             set.ContainsKey(3).Should(Be.False);
+        }
+
+        [Test]
+        public void ShouldBeEnumerableCollectionOfKeyValuePairs() {
+            var set = new MultiMapSet<int, string> {{1, "a"}, {2, "b"}};
+            foreach (var mapPair in set) {
+                mapPair.Should(Be.InstanceOf<KeyValuePair<int, HashSet<string>>>());
+            }
+        }
+
+        [Test]
+        public void ShouldRemoveEntryBasedOnKey() {
+            var set = new MultiMapSet<string, int> {{"foo", 42}};
+            set.Remove("foo").Should(Be.True);
+            set.Should(Be.Empty);
+        }
+
+        [Test]
+        public void RemovingNonExistentKeyShouldReturnFalse() {
+            var set = new MultiMapSet<string, int> {{"foo", 42}};
+            set.Remove("42").Should(Be.False);
+            set.ShouldNot(Be.Empty);
         }
     }
 }
--- Ruhe/MultiMapSet.cs
+++ Ruhe/MultiMapSet.cs
@@ -1,7 +1,8 @@
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Ruhe {
-    public class MultiMapSet<TKey, TValueInSet> {
+    public class MultiMapSet<TKey, TValueInSet> : IEnumerable<KeyValuePair<TKey, HashSet<TValueInSet>>> {
         private readonly IDictionary<TKey, HashSet<TValueInSet>> map = new Dictionary<TKey, HashSet<TValueInSet>>();
 
         public IEnumerable<TKey> Keys {
@@ -26,6 +27,18 @@ namespace Ruhe {
 
         public bool ContainsKey(TKey key) {
             return map.ContainsKey(key);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return map.GetEnumerator();
+        }
+
+        public IEnumerator<KeyValuePair<TKey, HashSet<TValueInSet>>> GetEnumerator() {
+            return map.GetEnumerator();
+        }
+
+        public bool Remove(TKey key) {
+            return map.Remove(key);
         }
     }
 }
