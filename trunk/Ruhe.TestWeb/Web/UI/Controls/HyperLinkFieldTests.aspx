9307e374912f9213d934a47cedd67107ed0ef8fb
mikehale <mikehale@d311d79a-a41b-0410-be46-71240311c8d0>
2009-10-23 14:59:46 +0000

    Added a HyperLinkField which understands OGNL-like navigation to properties.

---
+++ trunk/Ruhe.TestWeb/Web/UI/Controls/HyperLinkFieldTests.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HyperLinkFieldTests.aspx.cs" Inherits="Ruhe.TestWeb.Web.UI.Controls.HyperLinkFieldTests" %>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" >
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="grid" runat="server" AutoGenerateColumns="false">
+            <Columns>
+                <ruhe:HyperLinkField HeaderText="ID" DataNavigateUrlFormatString="~/HyperLinkField.aspx?Id={0}" DataNavigateUrlFields="Bar.Id" DataTextField="Bar.Id" />
+            </Columns>
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ trunk/Ruhe/Web/UI/Controls/HyperLinkField.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using LiquidSyntax.ForWeb;
+
+namespace Ruhe.Web.UI.Controls {
+    /// <summary>
+    /// Enables OGNL-like navigation to properties. It does not yet support method
+    /// calls or other operations.
+    /// </summary>
+    public class HyperLinkField : System.Web.UI.WebControls.HyperLinkField {
+        protected object[] GetNavigateUrlValue(object dataItem) {
+            return DataNavigateUrlFields.Select(url => DataBinder.Eval(dataItem, url)).ToArray();
+        }
+
+        protected object GetTextValue(object dataItem) {
+            return DataBinder.Eval(dataItem, DataTextField);
+        }
+
+        public override void InitializeCell(DataControlFieldCell cell, DataControlCellType cellType, DataControlRowState rowState, int rowIndex) {
+            base.InitializeCell(cell, cellType, rowState, rowIndex);
+            ReplaceDataBindingEvent(cell);
+        }
+
+        protected void OnDataBindField(object sender, EventArgs e) {
+            var boundControl = (HyperLink) sender;
+            var controlContainer = boundControl.NamingContainer;
+            var dataItem = DataBinder.GetDataItem(controlContainer);
+
+            boundControl.Text = FormatDataTextValue(GetTextValue(dataItem));
+            boundControl.NavigateUrl = FormatDataNavigateUrlValue(GetNavigateUrlValue(dataItem));
+        }
+
+        protected void ReplaceDataBindingEvent(Control cell) {
+            var oldLink = cell.FindFirst<HyperLink>();
+            if (oldLink == null) return;
+
+            cell.Controls.Remove(oldLink);
+            var newLink = new HyperLink {Text = oldLink.Text, NavigateUrl = oldLink.NavigateUrl, Target = oldLink.Target};
+            newLink.DataBinding += OnDataBindField;
+            cell.Controls.Add(newLink);
+        }
+    }
+}
