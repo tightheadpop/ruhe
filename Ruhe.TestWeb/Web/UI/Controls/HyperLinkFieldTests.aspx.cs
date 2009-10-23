89fc1e535f79ca4784e0e2c1b4b0c9719ff38e96
mikehale <mikehale@d311d79a-a41b-0410-be46-71240311c8d0>
2009-10-23 14:59:46 +0000

    Added a HyperLinkField which understands OGNL-like navigation to properties.

---
+++ Ruhe.TestWeb/Web/UI/Controls/HyperLinkFieldTests.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.UI;
+using LiquidSyntax;
+
+namespace Ruhe.TestWeb.Web.UI.Controls {
+    public partial class HyperLinkFieldTests : Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            grid.DataSource = new Baz().AsList();
+            grid.DataBind();
+        }
+
+        public class Bar {
+            public int Id {
+                get { return 1; }
+            }
+        }
+
+        public class Baz {
+            public Bar Bar {
+                get { return new Bar(); }
+            }
+        }
+    }
+}
---
+++ Ruhe.Tests/Web/UI/Controls/HyperLinkFieldTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Extensions.Asp.HtmlTester;
+using NUnit.Framework;
+using Ruhe.Web.UI.Controls;
+
+namespace Ruhe.Tests.Web.UI.Controls {
+    [TestFixture]
+    public class HyperLinkFieldTests : RuheWebTest<HyperLinkField> {
+        [Test]
+        public void HyperLinkField() {
+            LoadPage();
+            var hyperLinkField = new HtmlAnchorTester("//a[1]", "the first link on the page, which is the hyperlinkfield");
+            StringAssert.EndsWith("HyperLinkField.aspx?Id=1", hyperLinkField.HRef);
+            Assert.AreEqual("1", hyperLinkField.InnerHtml);
+        }
+    }
+}
