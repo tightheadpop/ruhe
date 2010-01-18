7e789212f18d796a495f9aa3167bc9a584cefeb8
matthew.a.frank <matthew.a.frank@d311d79a-a41b-0410-be46-71240311c8d0>
2010-01-18 21:43:26 +0000

    added MsBuild task for NVelocity template processing

---
+++ trunk/Ruhe.Tests/MsBuild/ApplyTemplateTaskTests.cs
@@ -0,0 +1,45 @@
+using System.IO;
+using System.Reflection;
+using LiquidSyntax;
+using Microsoft.Build.Utilities;
+using NUnit.Framework;
+using Ruhe.MsBuild;
+using LiquidSyntax.ForTesting;
+
+namespace Ruhe.Tests.MsBuild {
+    [TestFixture]
+    public class ApplyTemplateTaskTests {
+        private static readonly string outputFile = Path.GetTempFileName();
+        private static readonly string templateFile = GetResourceName("Template.txt");
+        private static readonly string propertiesFile = GetResourceName("Properties.txt");
+        private Task task;
+
+        [Test]
+        public void CreatesOutputFileWithSubstitutionsMade() {
+            task.Execute();
+            File.ReadAllText(outputFile).Should(Be.EqualTo(@"Old McDonald had a farm.
+On that farm, he had a cow that says ""moo."""));
+        }
+
+        [Test]
+        public void ReturnsTrueIfSuccessful() {
+            Assert.IsTrue(task.Execute());
+        }
+
+        [SetUp]
+        public void SetUp() {
+            task = new ApplyTemplateTask {TemplateFile = templateFile, PropertiesFile = propertiesFile, OutputFile = outputFile};
+        }
+
+        [TearDown]
+        public void TearDown() {
+            File.Delete(outputFile);
+        }
+
+        private static string GetResourceName(string shortName) {
+            var uri = Assembly.GetExecutingAssembly().CodeBase;
+            var path = Path.GetDirectoryName(uri.WithoutPrefix("file:///").Replace('/', '\\'));
+            return Path.Combine(path, @"..\..\Resources\" + shortName);
+        }
+    }
+}
---
+++ trunk/Ruhe/MsBuild/ApplyTemplateTask.cs
@@ -0,0 +1,52 @@
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using NVelocity;
+using NVelocity.App;
+
+namespace Ruhe.MsBuild {
+    public class ApplyTemplateTask : Task {
+        [Required]
+        public string TemplateFile { get; set; }
+
+        [Required]
+        public string PropertiesFile { get; set; }
+
+        [Required]
+        public string OutputFile { get; set; }
+
+        public override bool Execute() {
+            try {
+                using (var templateStream = new FileStream(TemplateFile, FileMode.Open, FileAccess.Read)) {
+                    var context = GetInitializedContext();
+                    ApplyTemplate(templateStream, context);
+                }
+                return true;
+            }
+            catch {
+                return false;
+            }
+        }
+
+        private VelocityContext GetInitializedContext() {
+            var context = new VelocityContext();
+            using (var propertiesStream = new StreamReader(PropertiesFile)) {
+                string line;
+                while (null != (line = propertiesStream.ReadLine())) {
+                    var pair = line.Split('=');
+                    if (pair.Length < 2) continue;
+                    context.Put(pair[0].Trim(), pair[1].Trim());
+                }
+            }
+            return context;
+        }
+
+        public void ApplyTemplate(Stream templateStream, VelocityContext context) {
+            var velocity = new VelocityEngine();
+            velocity.Init();
+            using (var writer = new StreamWriter(OutputFile, false)) {
+                velocity.Evaluate(context, writer, "MsBuild", new StreamReader(templateStream));
+            }
+        }
+    }
+}
